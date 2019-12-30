60b4195bd7914131a1e0d6de4a31c5eb9b91dd94
Kadeem Jackson <kdjackson35@gmail.com>
2019-12-30 17:58:32 -0500

    Uploaded Classwork

---
+++ W6D1/polytree_node/lib/00_tree_node.rb
@@ -0,0 +1,52 @@
+class PolyTreeNode
+
+    attr_reader :parent, :value 
+    attr_accessor :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    
+    def parent=(node = nil)
+
+        if !self.parent.nil?
+            idx = self.parent.children.index(self)
+            self.parent.children.delete_at(idx)
+        end
+
+        @parent = node
+        
+        if !node.nil?
+          node.children << self unless node.children.include?(self)
+        end
+    end
+
+    def add_child(node) # node = child
+
+        node.parent = self
+
+        self.children << node if !self.children.include?(node)
+    end
+
+    def remove_child(node)
+        
+        raise "this node is not a child" unless self.children.include?(node)
+        
+        node.parent = nil
+    end
+
+    def dfs(target)
+        return self if self.value == target
+
+        self.children.each do |child|
+            results = child.dfs(target)
+            return results if !results.nil?
+        end 
+        nil
+    end
+
+    
+end
