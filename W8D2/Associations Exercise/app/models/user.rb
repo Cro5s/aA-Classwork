622c68d0515642e8e43ea380479a48d216a2638a
Kadeem Jackson <kdjackson35@gmail.com>
2020-01-14 22:00:50 -0500

    Associations Exercise update

--- W8D2/Associations Exercise/app/models/user.rb
+++ W8D2/Associations Exercise/app/models/user.rb
@@ -1,2 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ApplicationRecord
+  has_many :enrollments,
+    class_name: "Enrollment",
+    primary_key: :id,
+    foreign_key: :student_id
+
+  has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
+
+  has_many :taught_courses,
+    class_name: "Course",
+    primary_key: :id,
+    foreign_key: :instructor_id 
 end
