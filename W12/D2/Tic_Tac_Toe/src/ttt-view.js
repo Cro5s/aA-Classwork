23455874ec4407c792a321d0bf27f67ad172076e
Kadeem Jackson <kdjackson35@gmail.com>
2020-02-11 14:28:48 -0500

    CSS with friends Part 9 && TicTacToe jQuery

---
+++ W12/D2/Tic_Tac_Toe/src/ttt-view.js
@@ -0,0 +1,14 @@
+class View {
+  constructor(game, $el) {};
+
+  bindEvents() {};
+
+  makeMove($square) {};
+
+  setupBoard() {
+    const $grid = $("<ul></ul>");
+    const $cell = $("<li></li>")
+  };
+}
+
+module.exports = View;
