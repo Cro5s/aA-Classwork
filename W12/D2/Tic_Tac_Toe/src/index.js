060faadbb817ede385c556b6235403ddcca61967
Kadeem Jackson <kdjackson35@gmail.com>
2020-02-11 20:00:45 -0500

    Tic Tac Toe jQuery and CSS

--- W12/D2/Tic_Tac_Toe/src/index.js
+++ W12/D2/Tic_Tac_Toe/src/index.js
@@ -1,5 +1,5 @@
 const View = require("./ttt-view");
-const Game = require("../../Tic_Tac_Toe_Node_Solutions/game");
+const Game = require("../Tic_Tac_Toe_Node_Solutions/game");
 
 const view = new View(); 
 const game = new Game();
@@ -7,5 +7,5 @@ const game = new Game();
 $(() => {
   const $el = $(".ttt");
   const newGame = new Game();
-  const newView = new View(newGame, $el);
+  new View(newGame, $el);
 });
